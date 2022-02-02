610f2b047561ca12c9a74788b2d6e4cb746a6ca1
Regilan <mariairegilan@gmail.com>
2022-02-02 20:49:57 +0530

    chore: update swagger

---
+++ src/main/java/com/admmprayergroup/assistant/controllers/DefaultController.java
@@ -0,0 +1,20 @@
+package com.admmprayergroup.assistant.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class DefaultController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<Map<String, String>> introduction() {
+        Map<String, String> responseValue = new HashMap<>();
+        responseValue.put("swaggerURL", "http://localhost:8080/swagger-ui.html");
+        return ResponseEntity.ok(responseValue);
+    }
+
+}
